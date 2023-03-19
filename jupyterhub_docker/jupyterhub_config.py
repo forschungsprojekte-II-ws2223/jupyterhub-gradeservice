70e71b99dd495c4c40cc107f67d12d69272c477f
Maximilian Schüle <max.schuele@gmail.com>
2023-03-19 14:11:45 +0100

    format jupyterhub config

--- jupyterhub_docker/jupyterhub_config.py
+++ jupyterhub_docker/jupyterhub_config.py
@@ -16,18 +16,18 @@ import os
 import sys
 
 c = get_config()  # pyright: reportUndefinedVariable=false
-c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
-c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_IMAGE']
 
 network_name = os.environ['DOCKER_NETWORK_NAME']
+notebook_dir = "/home/jovyan/work"
+
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+
+c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_IMAGE']
 c.DockerSpawner.use_internal_ip = True
 c.DockerSpawner.network_name = network_name
 c.DockerSpawner.extra_host_config = {'network_mode': network_name}
-
-notebook_dir = os.environ.get("DOCKER_NOTEBOOK_DIR") or "/home/jovyan/work"
 c.DockerSpawner.notebook_dir = notebook_dir
 c.DockerSpawner.volumes = {"jupyterhub-user-{username}": notebook_dir}
-
 c.DockerSpawner.remove = True
 #c.Spawner.mem_limit = '2G'
 
