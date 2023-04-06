19e4abb01fc35a31719b7b300796f77fb7fd583b
Yichi Zhang <yichizhang15@gmail.com>
2023-04-06 18:01:37 +0200

    make docker work with new filestructure

--- gradeservice/src/gradeservice/main.py
+++ gradeservice/src/gradeservice/main.py
@@ -1,26 +1,30 @@
 from fastapi import FastAPI, File, UploadFile
+from otter import assign, grade
 
 import os
 
 app = FastAPI()
 
+
 @app.post("/{course_id}/{activity_id}")
 async def create_upload_file(course_id: int, activity_id: int, file: UploadFile):
-    folder_path = f'submissions/{course_id}/{activity_id}'
+    folder_path = f"submissions/{course_id}/{activity_id}"
     os.makedirs(folder_path, exist_ok=True)
 
-    filepath = f'{folder_path}/{file.filename}'
+    filepath = f"{folder_path}/{file.filename}"
 
     try:
         contents = file.file.read()
-        with open(filepath, mode='wb') as f:
+        with open(filepath, mode="wb") as f:
             f.write(contents)
     except Exception:
         return {"message": "There was an error uploading the file"}
     finally:
         file.file.close()
 
-    stream = os.popen(f'otter assign {filepath} {folder_path}/dist')
+    stream = os.popen(f"otter assign {filepath} {folder_path}/dist")
+    # stream = os.popen(assign(filepath, folder_path))
     output = stream.read()
+    stream.close
 
     return {"message": output}
--- gradeservice/src/gradeservice/otter.py
+++ gradeservice/src/gradeservice/otter.py
@@ -1,8 +1,11 @@
 import subprocess
 
-def assign(path: str):
-    cmd = f'otter assign {path}/demo.ipynb {path}/dist'
+
+def assign(file_path: str, folder_path):
+    cmd = f"otter assign {file_path}/demo.ipynb {folder_path}/dist"
     return subprocess.run([cmd], shell=True, capture_output=True, text=True)
 
-def grade():
-    return 0
+
+def grade(submission_path: str, autograder_path: str):
+    cmd = f"otter grade -p {submission_path} -a {autograder_path}_*.zip --pdfs -v"
+    return subprocess.run([cmd], shell=True, capture_output=True, text=True)
