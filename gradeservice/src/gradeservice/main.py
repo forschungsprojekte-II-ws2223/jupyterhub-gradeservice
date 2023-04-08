eaefdd942543fd3734ebe5bb34f422aabd5d9f07
Maximilian Schüle <max.schuele@gmail.com>
2023-04-09 01:11:34 +0200

    add router & TestClient

--- gradeservice/src/gradeservice/main.py
+++ gradeservice/src/gradeservice/main.py
@@ -1,24 +1,11 @@
-import os
-
-from fastapi import FastAPI, HTTPException, UploadFile
+from fastapi import FastAPI
 
 import gradeservice.otter as otter
 
 app = FastAPI()
 
+app.include_router(otter.router)
 
-@app.post("/{course_id}/{activity_id}")
-async def create_assignment(course_id: int, activity_id: int, file: UploadFile):
-    try:
-        res = await otter.create_assignment(course_id, activity_id, file)
-    except OSError:
-        raise HTTPException(
-            status_code=400, detail=f"Activity {course_id}/{activity_id} already exists."
-        )
-    os.path.exists(123)
-    return {"message": res}
-
-
-@app.get("/{course_id}/{activity_id}")
-async def get_assignment(course_id: int, activity_id: int):
-    return 0
+@app.get("/")
+async def root():
+    return {"msg": "Hello World"}
--- gradeservice/src/gradeservice/otter.py
+++ gradeservice/src/gradeservice/otter.py
@@ -1,37 +1,39 @@
 import base64
 import os
-import pathlib
 import subprocess
+from pathlib import Path
+from subprocess import CalledProcessError
 
-from fastapi import UploadFile
+from fastapi import APIRouter, HTTPException, UploadFile
 
+router = APIRouter()
 
+@router.post("/{course_id}/{activity_id}")
 async def create_assignment(course_id: int, activity_id: int, file: UploadFile):
-    path = pathlib.Path("assignments" , course_id, activity_id)
-    os.makedirs(path)
+    path = Path(f'assignments/{course_id}/{activity_id}')
+
+    try:
+        os.makedirs(path)
+    except OSError:
+        raise HTTPException(status_code=400, detail=f"Activity {course_id}/{activity_id} already exists.")
 
     file_path = path.joinpath(file.filename)
+    if file_path.suffix != ".ipynb":
+        raise HTTPException(status_code=400, detail=f'The file {file.filename} is not a .ipynb file.')
+
     contents = await file.read()
-    with open(file_path, "wb") as f:
-        f.write(contents)
-    await file.close()
 
-    res = subprocess.run(
-        [f"otter assign {file_path} {path}"],
-        shell=True,
-        capture_output=True,
-        text=True,
-    )
+    with open(file_path, "wb") as fp:
+        fp.write(contents)
 
-    f = open(f"assignments/{course_id}/{activity_id}/dist/student/demo.ipynb", "r")
+    try:
+        subprocess.run(
+            [f"otter assign -v {file_path} {path}"],
+            shell=True,
+            capture_output=True,
+            check=True,
+        )
+    except CalledProcessError as e:
+        raise HTTPException(status_code=400, detail=e.stderr)
 
-
-def get_assignment(course_id: int, activity_id: int):
-    if not os.path.exists(f"assignments/{course_id}/{activity_id}"):
-        return 0
-
-    return 0
-
-
-def grade_assignment():
-    return 0
+    return {"message": "success"}
