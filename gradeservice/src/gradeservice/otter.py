cb7b552ce810bd330df715ada83ad783af947f9b
Yichi Zhang <yichizhang15@gmail.com>
2023-04-09 17:44:31 +0200

    refactoring

--- gradeservice/src/gradeservice/otter.py
+++ gradeservice/src/gradeservice/otter.py
@@ -52,28 +52,28 @@ async def submit_upload_file(course_id: int, activity_id: int, file: UploadFile)
     submission_path = f"assignment/{course_id}/{activity_id}/submissions"
     os.makedirs(submission_path, exist_ok=True)
 
-    submissionpath = f"{submission_path}/{file.filename}"
+    file_path = submission_path.joinpath(file.filename)
 
     content = await file.read()
 
     try:
-        with open(submissionpath, mode="wb") as f:
+        with open(file_path, mode="wb") as f:
             f.write(content)
-    except Exception:
-        return {"message": "There was an error uploading the file"}
+    except HTTPException:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="Could not read file content")
     finally:
         file.file.close()
 
-    return {"message": "Submitted"}
+    return {"message": "success"}
 
 
 # grades the submissions, needs to be tested
 @router.get("/grade/{course_id}/{activity_id}")
 async def grade(course_id: int, activity_id: int):
     grading_path = f"submissions/{course_id}/{activity_id}/dist/autograder/demo-autograder"
-    filepath = f"submissions/{course_id}/{activity_id}/submissions"
+    files_path = f"submissions/{course_id}/{activity_id}/submissions"
 
-    stream = os.popen(f"otter grade -p {filepath} -a {grading_path}_*.zip --pdfs -v")
+    stream = os.popen(f"otter grade -p {files_path} -a {grading_path}_*.zip --pdfs -v")
     stream.close
 
     return {"message": "graded"}
