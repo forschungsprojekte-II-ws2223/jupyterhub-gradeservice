b1c97eb7fc734a217cc2078617fba1a2d8aa52ab
Maximilian Schüle <max.schuele@gmail.com>
2023-05-09 10:21:58 +0200

    restructure

--- jupyterhub-gradeservice/gradeservice/src/gradeservice/main.py
+++ jupyterhub-gradeservice/gradeservice/src/gradeservice/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseSettings
 
 import gradeservice.otter as otter
+
+
+class Settings(BaseSettings):
+    assignments_path: str
+
 
 app = FastAPI()
 
