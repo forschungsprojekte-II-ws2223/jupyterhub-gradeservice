43a04c2c6a40f83be70bb0cca63a0655e8c4bdec
Maximilian Schüle <max.schuele@gmail.com>
2023-06-18 11:12:52 +0200

    update deps

--- down.bat
+++
@@ -1,5 +0,0 @@
-: Removes jupyterhub, jupyterhub-db and gradeservice containers and volumes
-docker compose down -v
-
-: Removes user volumes
-docker volume rm $(docker volume ls -f name=jupyterhub-user -q)
